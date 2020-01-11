1e3998e2dda6c9ad2516356a6e60321188214553
Artem <onedeadwave@gmail.com>
2020-01-11 14:34:28 +1000

    Added fileserver

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"time"
 
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.New()
+
+	// Middleware
+	router.Use(gin.Logger())
+
+	// Frontend
+	router.Use(static.Serve("/", static.LocalFile("client/dist", true)))
+	router.LoadHTMLGlob("client/dist/*.html")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	server := &http.Server{
+		Handler: router,
+		Addr: ":8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout: 15 * time.Second,
+	}
+
+	log.Printf("Server started at port %v\n", server.Addr)
+	log.Fatal(server.ListenAndServe())
 }
